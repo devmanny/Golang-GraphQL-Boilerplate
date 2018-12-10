a8c14d0e4a56336716e02837a7f8514c007d7d52
Manuel Vargas <im@devmanny.io>
2018-12-09 20:04:25 -0600

    testung graphql

--- server/api/user/user.controller.go
+++ server/api/user/user.controller.go
@@ -2,87 +2,136 @@ package user
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"strconv"
 
 	"github.com/graphql-go/graphql"
 	"github.com/labstack/echo"
-	"google.golang.org/appengine"
 	"google.golang.org/appengine/datastore"
 )
 
-// var schema graphql.Schema
-
-// Init ...
-func Init() {
-	fmt.Println("INIT - - - -")
-
+// RequestData ...
+type RequestData struct {
+	Query     string                 `json:"query" query:"query"`
+	Variables map[string]interface{} `json:"variables" query:"variables"`
 }
 
-// Index ...
-func Index(c echo.Context) error {
+// // DemoGraphQL ...
+// func DemoGraphQL(c echo.Context) error {
 
-	// rootMutation ...
-	rootMutation := graphql.NewObject(graphql.ObjectConfig{
-		Name: "rootMutation",
-		Fields: graphql.Fields{
-			"createUser": &graphql.Field{
-				Type: UserType,
-				Args: graphql.FieldConfigArgument{
-					"name": &graphql.ArgumentConfig{
-						Type: graphql.NewNonNull(graphql.String),
-					},
-				},
-				Resolve: createUser,
+// 	// body, err := ioutil.ReadAll(c.Request().Body)
+
+// 	fields := graphql.Fields{
+// 		"hello": &graphql.Field{
+// 			Type: graphql.String,
+// 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+// 				fmt.Println(31, p.Args)
+// 				return "world", nil
+// 			},
+// 		},
+// 	}
+
+// 	rootQuery := graphql.ObjectConfig{
+// 		Name:   "RootQuery",
+// 		Fields: fields,
+// 	}
+
+// 	schemaConfig := graphql.SchemaConfig{
+// 		Query: graphql.NewObject(rootQuery),
+// 	}
+
+// 	schema, err := graphql.NewSchema(schemaConfig)
+
+// 	request := new(RequestData)
+// 	c.Bind(request)
+
+// 	fmt.Println(request)
+
+// 	params := graphql.Params{
+// 		Schema:        schema,
+// 		RequestString: request.Query,
+// 	}
+
+// 	resp := graphql.Do(params)
+
+// 	return c.JSON(http.StatusOK, resp)
+// }
+
+// GraphQL ...
+func GraphQL(c echo.Context) error {
+
+	var (
+
+		// UserType ...
+		UserType = graphql.NewObject(graphql.ObjectConfig{
+			Name: "User",
+			Fields: graphql.Fields{
+				"id":   &graphql.Field{Type: graphql.String},
+				"name": &graphql.Field{Type: graphql.String},
 			},
-		},
+		})
+
+		// UserArgs ...
+		UserArgs = graphql.FieldConfigArgument{
+			"name": &graphql.ArgumentConfig{
+				Type: graphql.NewNonNull(graphql.String),
+			},
+		}
+
+		// UserMutations ...
+		UserMutations = graphql.Fields{
+			"createUser": &graphql.Field{
+				Type:    UserType,
+				Args:    UserArgs,
+				Resolve: CreateUser,
+			},
+		}
+
+		// Mutation ...
+		Mutation = graphql.NewObject(graphql.ObjectConfig{
+			Name:   "Mutations",
+			Fields: UserMutations,
+		})
+
+		// Schema ...
+		Schema, _ = graphql.NewSchema(graphql.SchemaConfig{
+			Mutation: Mutation,
+		})
+	)
+
+	request := new(RequestData)
+	c.Bind(request)
+
+	fmt.Println(UserArgs.name)
+
+	response := graphql.Do(graphql.Params{
+		Schema:        Schema,
+		RequestString: request.Query,
+		// Context:       appengine.NewContext(c.Request()),
 	})
 
-	fmt.Println(rootMutation)
-
-	schema, err := graphql.NewSchema(graphql.SchemaConfig{
-		Mutation: rootMutation,
-	})
-
-	fmt.Println(err)
-
-	ctx := appengine.NewContext(c.Request())
-	body, err := ioutil.ReadAll(c.Request().Body)
-
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, err)
-	}
-
-	query := string(body)
-
-	fmt.Println(schema, query, ctx)
-
-	resp := graphql.Do(graphql.Params{
-		Schema:        schema,
-		RequestString: query,
-	})
-
-	if len(resp.Errors) > 0 {
-		return c.JSON(http.StatusBadRequest, resp.Errors)
-	}
-
-	return c.JSON(http.StatusOK, resp)
+	return c.JSON(http.StatusOK, response)
 }
 
-func createUser(params graphql.ResolveParams) (interface{}, error) {
+// CreateUser ...
+func CreateUser(params graphql.ResolveParams) (interface{}, error) {
 	ctx := params.Context
 
 	name, _ := params.Args["name"].(string)
 	user := &User{Name: name}
 	key := datastore.NewIncompleteKey(ctx, "User", nil)
 
+	fmt.Println(81, name)
+
 	// Insert user into Datastore
-	if generatedKey, err := datastore.Put(ctx, key, user); err != nil {
+	generatedKey, err := datastore.Put(ctx, key, user)
+
+	if err != nil {
 		return User{}, err
-	} else {
-		// Set user's auto-generated ID
-		user.ID = strconv.FormatInt(generatedKey.IntID(), 10)
-		return user, nil
 	}
+
+	// Set user's auto-generated ID
+	user.ID = strconv.FormatInt(generatedKey.IntID(), 10)
+	return user, nil
+
 }
