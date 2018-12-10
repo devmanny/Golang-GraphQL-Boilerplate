b917cc8cac2776fae3341a322a232166a782f427
Manuel Vargas <im@devmanny.io>
2018-12-09 20:04:04 -0600

    routes to graphql

--- server/routes.go
+++ server/routes.go
@@ -6,16 +6,14 @@ import (
 	"app.onca.api/server/api/thing"
 	"app.onca.api/server/api/user"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 )
 
 // ConfigureRoutes ...
 func ConfigureRoutes() *echo.Echo {
-	e := echo.New()
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
 
-	apiV1 := e.Group("/api/v1") // se puede omitir la versión
+	e := echo.New()
+
+	apiV1 := e.Group("/graphql/v1") // se puede omitir la versión
 
 	user.Router(apiV1.Group("/users"))
 	thing.Router(apiV1.Group("/things"))
