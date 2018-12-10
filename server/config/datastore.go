694cb2bd2e995c733f7065381366f25b7f77d409
Manuel Vargas <im@devmanny.io>
2018-12-10 00:32:03 -0600

    removing comented code

--- server/config/datastore.go
+++ server/config/datastore.go
@@ -4,26 +4,10 @@ import (
 	"fmt"
 )
 
-// DatastoreClient ...
-
 // Datastore config...
 func Datastore() {
 
-	// ctx := context.Background()
-	// projID := os.Getenv("PROJECT_ID")
-	// if projID == "" {
-	// 	log.Fatal(`You need to set the environment variable "DATASTORE_PROJECT_ID"`)
-	// }
-
-	// datastoreClient, err := datastore.NewClient(ctx, projID)
-
-	// if err != nil {
-	// 	log.Fatalf("Could not create datastore client: %v", err)
-	// }
-
 	fmt.Println("Cloud Datastore OMG")
 	fmt.Println()
-
-	// return datastoreClient
 
 }
