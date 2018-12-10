2afbfc1dd554c3dcfceafbaf94a129324913ae7e
Manuel Vargas <im@devmanny.io>
2018-12-09 20:04:53 -0600

    clean schema

--- server/api/user/user.schema.go
+++ server/api/user/user.schema.go
@@ -1,18 +1,7 @@
 package user
 
-import "github.com/graphql-go/graphql"
-
 // User ...
 type User struct {
 	ID   string `json:"id" datastore:"-"`
 	Name string `json:"name"`
 }
-
-// UserType ...
-var UserType = graphql.NewObject(graphql.ObjectConfig{
-	Name: "User",
-	Fields: graphql.Fields{
-		"id":   &graphql.Field{Type: graphql.String},
-		"name": &graphql.Field{Type: graphql.String},
-	},
-})
